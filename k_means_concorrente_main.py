94e94de3486af27c24c5a4d6eb2da88d9fc8e81d
Igor Santos <igordejesussantos@hotmail.com>
2024-11-23 08:52:59 -0300

    feat: implementado k-means sequencial e concorrente

---
+++ k_means_concorrente_main.py
@@ -0,0 +1,176 @@
+import sys
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from multiprocessing import (Process, Semaphore, Value, Lock, Event)
+from k_means_concorrente import *
+from PyQt5.QtWidgets import QApplication,QWidget,QInputDialog
+
+# Variáveis
+num_processos = 8
+sem_m = Semaphore(0) # Semáforo para controle da main esperar os processos
+sem_c = Semaphore(0) # Semáforo para os processos esperarem a main liberá-los para próxima rodada, verificando antes a stop_condition
+lock = Lock() # Lock para garantir que o contador é atualizado de forma atômica
+lock_soma = Lock() # Lock para garantir que os processos não terão condição de corrida nos dados dos centroides sendo atualizados
+counter = Value('i', 0)  # Contador de processos que terminaram a rodada
+stop_condition = Value('i', 0)  # Condição de parada
+c = 0 # Contador de iterações/rodadas do algoritmo
+
+
+class App(QWidget):
+    # Define as configurações das janelas 
+    def __init__(self):
+        super().__init__()
+        self.title='K-mean clustering'
+        self.left=10
+        self.top=10
+        self.width=300
+        self.height=250
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left,self.top,self.width,self.height)
+        self.getText()
+        self.show()
+    def getText(self):
+        global c
+        end = False
+        list_col = []
+        # d1,okPressed1=QInputDialog.getText(self, "Arquivo", "Caminho") # Caminho do arquivo csv com os dados
+        # d2,okPressed2=QInputDialog.getInt(self, "Centroides", "Qtde:") # Obtém número de clusters/centróides
+        
+        # while(not end):
+        #     d4,okPressed4=QInputDialog.getText(self, "Nova Coluna", "Nome")
+
+        #     if d4 == "" and okPressed4:
+        #         end = True
+        #     else:
+        #         list_col.append(d4)
+
+        # Código temporário apenas para facilitar testes
+        # Posteriormente descomentar linhas de código acima e excluir esse trecho
+        okPressed1 = True
+        okPressed2 = True
+        d1 = "./datasets/Iris.csv"
+        d2 = 3
+        list_col = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm"]
+
+        if okPressed1 and okPressed2:
+            # Ler csv com pandas
+            df = pd.read_csv(d1, delimiter="," )
+
+            # Transforma o index em uma coluna também para rastrear posteriormente as instâncias e converte para numpy array
+            df["centroide"] = 0
+            df = df.reset_index(drop=False)
+            list_col.append("centroide")
+            list_col.append("index")
+            data = df[list_col].to_numpy()
+
+            # Num de linhas
+            num_linhas = data.shape[0]
+
+            # Obtém os centróides iniciais utilizando as primeiras linhas e ignorando a coluna de index (centróides não são instâncias) e a qual centroide pertence
+            # Além disso temos o dobro de linhas onde o segundo bloco de linhas será utilizado para cálculo da próxima rodada com a nova
+            # posição dos centróides
+            tuple_zeros = tuple(0 for i in range(len(list_col) - 1))
+
+            list_for_centers = [tuple(data[i, :-1]) for i in range(d2)]
+            list_for_centers = list_for_centers + [tuple_zeros for i in range(d2)]
+
+            cluster_centers = np.array(list_for_centers)
+
+            # Num cols centroides
+            num_cols_centroides = cluster_centers.shape[1]
+
+            # Criando memória compartilhada para o np array dos centróides
+            shm1 = shared_memory.SharedMemory(create=True, size=cluster_centers.nbytes)
+            shared_array1 = np.ndarray(cluster_centers.shape, dtype=cluster_centers.dtype, buffer=shm1.buf)
+            np.copyto(shared_array1, cluster_centers)  # Copia os dados para a memória compartilhada
+
+            # Cria memória compartilhada para o np array dos dados
+            shm2 = shared_memory.SharedMemory(create=True, size=data.nbytes)
+            shared_array2 = np.ndarray(data.shape, dtype=data.dtype, buffer=shm2.buf)
+            np.copyto(shared_array2, data)  # Copia os dados para a memória compartilhada
+
+            # Calcula a quantidade de linhas para cada processo com divisão inteira. O resto será atribuido ao último processo.
+            step = num_linhas // num_processos
+
+
+            processos = []
+            # Cria e dispara os processos
+            for i in range(num_processos):
+                start_row = i * step
+                end_row = (i + 1) * step if (i < num_processos - 1) else num_linhas
+                p = Process(target=Calculadores, 
+                            args=(i, shm1.name, shm2.name, cluster_centers.shape, data.shape, cluster_centers.dtype, data.dtype, start_row, end_row, num_processos, d2, lock, lock_soma, counter, sem_m, sem_c, stop_condition))
+                processos.append(p)
+                p.start()
+
+
+            while stop_condition.value == 0:
+                # Main Aguardando todos os processos completarem a rodada.
+        
+                # Espera até que o último processo avise a main
+                sem_m.acquire()
+
+                # Atualizar valor dos novos centroides
+                for i in range(d2, 2*d2):
+                    for j in range(num_cols_centroides - 1):
+                         shared_array1[i, j] = round(shared_array1[i, j] / shared_array1[i, -1], 5)
+
+                # Verifica se os centroides atualizados são iguais aos anteriores
+                change_flag = False
+                for i in range(0, d2):
+                    for j in range(num_cols_centroides - 1):
+                        if shared_array1[i, j] != shared_array1[i+d2, j]:
+                            change_flag = True
+                            break
+                    if change_flag:
+                        break
+
+                # Se não houve mudança altera a stop_condition
+                if change_flag == False:
+                    stop_condition.value = 1
+                else:
+                    # Se houve mudança copia os valores dos centroides atualizados para a parte de cima
+                    for i in range(0, d2):
+                        for j in range(num_cols_centroides - 1):
+                            shared_array1[i, j] = shared_array1[i+d2, j]
+                            shared_array1[i+d2, j] = 0 # Zera as posições dos centroides
+                        shared_array1[i+d2, -1] = 0 # Zera coluna da quantidade de pontos que pertencem ao centroide
+                
+
+                c = c + 1 # Conta número de iterações/rodadas que o algoritmo teve
+                for i in range(num_processos):
+                    sem_c.release()
+
+            print("Main terminou execução!")
+
+            # Espera todos os processos terminarem
+            for p in processos:
+                p.join()
+
+            print(f"Algoritmo finalizou com {c} iterações")
+
+            # Salva csv dos centroides
+            df_tmp = pd.DataFrame(shared_array1[:d2,:-1],  columns = list_col[0:-2])
+            df_tmp.to_csv("./logs/Concorrente/log0", index = False)
+
+            # Salva csv dos dados com as colunas de qual centroide pertence e seu índice no arquivo original
+            df_tmp = pd.DataFrame(shared_array2,  columns = list_col)
+            df_tmp.to_csv("./logs/Concorrente/log1", index = False)
+
+            # Fechar as referências à memória compartilhada
+            shm1.close()  # Fecha a referência ao shm1
+            shm2.close()  # Fecha a referência ao shm2
+
+            # Desvincular as memórias compartilhadas para liberar o nome no sistema
+            shm1.unlink()  # Desvincula shm1, liberando o nome da memória compartilhada
+            shm2.unlink()  # Desvincula shm2, liberando o nome da memória compartilhada
+
+            
+
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    ex=App()
---
+++ k_means_sequencial.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def calcula_dist(p1, p2):
+    """
+    :param p1: primeiro ponto
+    :param p2: segundo ponto
+    :return: distância entre o primeiro e segundo ponto
+    """
+    
+    soma = 0
+    # percorre todas os valores das colunas menos a do index
+    for i in range(p1.shape[0] - 1):
+        soma += (p1[i] - p2[i]) ** 2
+
+    return soma ** (1/2)
+
+
+def k_means(n_clusters, data):
+    """
+    :param n_clusters: número de clusters
+    :param data: dados
+    :return: dicionário onde a chave é o centroide e o valor associado os pontos próximos a ele
+    """
+    
+    qtde_cols = data.shape[1] # obtém a quantidade de colunas
+
+    # Obtendo centroides aleatórios
+    cluster_centers = set()
+    
+    # while len(cluster_centers) != n_clusters:
+    #     # Aqui são escolhidos "n_clusters" de pontos (instância ou linhas) aleatoriamente para serem os centróides iniciais
+    #     # O set é para garantir que pontos que potencialmente sejam sorteados mais de uma vez sejam adicionados mais de uma vez
+    #     cluster_centers = set([tuple(data[np.random.randint(0, data.shape[0]), :-1]) for _ in range(n_clusters)])
+
+    # cluster_centers = tuple(cluster_centers)
+
+    # Utilizando os primeirosdados como os primeiros "n_clusters" 
+    cluster_centers = set([tuple(data[i, :-1]) for i in range(n_clusters)])
+
+
+    # Listas para saber se as médias dos pontos mudaram a cada iteração
+    centers_before = None
+    centers_after = []
+    centers = None
+
+    while centers_before != centers_after:
+        centers_before = centers_after
+
+        # Dicionário de pontos por centroides
+        # Temos como chave a tupla com os valores 
+        centers = dict(zip(cluster_centers, [set() for _ in range(n_clusters)]))
+
+        # Associa cada ponto ao seu centroide mais próximo
+        for ponto in data:
+            min_center = None
+            min_dist = np.inf
+            for center in cluster_centers:
+                dist = calcula_dist(ponto, center)
+                if dist < min_dist:
+                    min_center = center
+                    min_dist = dist
+            centers[min_center].add(tuple(ponto))
+
+        # Cálculo os novos centroides
+        centers_after = []
+        for center in centers:
+            ponto_medio = []
+            for i in range(qtde_cols - 1): # verificar se é pra desconsiderar última coluna que é o index!!!!!!!!!!!!!!!!
+                soma = sum([col[i] for col in centers[center]]) #
+                media = round(soma / len(centers[center]), 5) # arredondadno número da coluna do contreóide em 5 casas decimais
+                ponto_medio.append(media)
+
+            centers_after.append(tuple(ponto_medio))
+
+        cluster_centers = tuple(centers_after)
+
+    return centers
+
+
+def plotar(data, result, cols):
+    if type(list(list(result.values())[0])[0]) == np.int64:
+        plt.hlines(1, 1, max(data) + 1)
+        plt.xlim(0, max(data) + 1)
+        plt.ylim(0.5, 1.5)
+
+        for item in result:
+            lista = list(result[item])
+            y = np.ones(np.shape(lista))
+            plt.plot(lista, y, '|', ms=40)
+        plt.axis('off')
+        plt.show()
+    else:
+        # plt.figure(figsize=(12, 12))
+        if data.shape[1] == 3:
+            ax = plt.axes(projection='3d')
+            ax.set_xlabel(cols[0])
+            ax.set_ylabel(cols[1])
+            ax.set_zlabel(cols[2])
+        else:
+            plt.xlabel(cols[0])
+            plt.ylabel(cols[1])
+        for item in result:
+            array = np.array(list(result[item]))
+            if array.shape[1] == 2:
+                plt.scatter(array[:, 0], array[:, 1])
+            else:
+                ax.scatter(array[:, 0], array[:, 1], array[:, 2])
+        centroides = np.array(list(result.keys()))
+        if centroides.shape[1] == 2:
+            plt.scatter(centroides[:, 0], centroides[:, 1], marker="D",s=60, c="red")
+        else:
+            ax.scatter(centroides[:, 0], centroides[:, 1], centroides[:, 2],marker="D", s=60, c="red")
+        plt.show()
